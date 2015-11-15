c0120342793c205843fbfb86c282060e95e01ee6
Chad McElligott <chad.mcelligott@gmail.com>
2015-11-14 22:29:41 -0600

    Refactor complex logic to function

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -2,12 +2,13 @@
   "a lackadaisical teenager"
   (:use [clojure.string :only [blank?, upper-case]]))
 
-(def response-for
-  (fn [input] 
-    (cond
-      (blank? input) "Fine. Be that way!" 
-      (and 
-        (= input (upper-case input)) 
-        (re-find #"[A-Z]" input)) "Whoa, chill out!"
-      (= \? (last input)) "Sure."
-      :default "Whatever.")))
+(defn yelling? [input]
+  true? (and (= input (upper-case input)) 
+             (re-find #"[A-Z]" input)))
+
+(defn response-for [input] 
+  (cond
+    (blank? input) "Fine. Be that way!" 
+    (yelling? input) "Whoa, chill out!"
+    (= \? (last input)) "Sure."
+    :default "Whatever."))
