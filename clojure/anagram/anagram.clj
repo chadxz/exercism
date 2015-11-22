8e1818998b9241474e0d00cecdbe81ced891e295
Chad McElligott <chad.mcelligott@gmail.com>
2015-11-22 14:47:27 -0600

    Improve anagram solution based on review

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -1,15 +1,13 @@
 (ns anagram
-  (:require [clojure.string :refer [lower-case]])
-  (:require [clojure.data :refer [diff]]))
+  (:require [clojure.string :refer [lower-case]]))
 
-(defn- exact-same-letters [word other]
-  (let [comparison (diff (frequencies word) (frequencies other))]
-    (and (= nil (nth comparison 0)) (= nil (nth comparison 1)))))
+(defn- same-letters? [word other]
+  (= (sort word) (sort other)))
 
-(defn- is-anagram [word other]
+(defn- is-anagram? [word other]
   (let [word (lower-case word) other (lower-case other)]
-    (and (not= word other) (exact-same-letters word other))))
+    (and (not= word other) (same-letters? word other))))
 
 (defn anagrams-for [word choices]
   "Return all items from `choices` that are anagrams of `word`."
-  (filter (partial is-anagram word) choices))
+  (filter (partial is-anagram? word) choices))
